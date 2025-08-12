8a82286e7f84b6284e3d65d61e080a26df7c4b4d
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-08-12 16:27:05 +0000

    Logo to'liq ko'rinmayaptio

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,5 +1,5 @@
 export const Logo = ({ isWhite = false }) => (
-  <svg width="140" height="40" viewBox="0 0 140 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="180" height="40" viewBox="0 0 180 40" fill="none" xmlns="http://www.w3.org/2000/svg">
     <text
       x="0"
       y="28"
