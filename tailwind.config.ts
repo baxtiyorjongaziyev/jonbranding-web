4b40acf42e797a4157415b6095eb1ca8d8722020
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-08-13 12:20:55 +0000

    tanlanmagan paketni animatsiyasi juda tez sal sekinlashtir
    Keyin chiqib

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,11 +89,9 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'subtle-pulse': 'subtle-pulse 4s ease-in-out infinite',
+        'subtle-pulse': 'subtle-pulse 8s ease-in-out infinite',
       },
     },
   },
   plugins: [require('tailwindcss-animate')],
 } satisfies Config;
-
-    
