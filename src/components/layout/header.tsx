08421e677504d52a607f971738ce7a7add3a3a38
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-01 16:25:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -120,7 +120,7 @@ const Header: FC = () => {
             </NavigationMenuItem>
             {navItems.map((item) => (
               <NavigationMenuItem key={item.label}>
-                <Link href={item.href} passHref legacyBehavior>
+                <Link href={item.href} legacyBehavior passHref>
                   <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
                     <a>{item.label}</a>
                   </NavigationMenuLink>
