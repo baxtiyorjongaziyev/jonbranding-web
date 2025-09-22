82f30dccf645969e6854f8375a453874ff24d131
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-22 13:28:17 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -136,7 +136,7 @@ const Header: FC = () => {
             </NavigationMenuItem>
             {navItems.map((item) => (
               <NavigationMenuItem key={item.label}>
-                <Link href={item.href} legacyBehavior passHref>
+                <Link href={item.href} passHref>
                   <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "bg-transparent", scrolled && "text-foreground hover:bg-black/10 hover:text-foreground focus:bg-black/10")}>
                     {item.label}
                   </NavigationMenuLink>
