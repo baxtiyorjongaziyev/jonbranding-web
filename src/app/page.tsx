780e43c4fdde560b586ca0838f83b4fe00818e3f
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-23 17:48:20 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,8 @@ export const metadata: Metadata = {
   description: "Biz shunchaki logotip chizmaymiz. Biz biznesingiz uchun natija keltiradigan, strategiyaga asoslangan va mijozlaringiz qalbidan joy oladigan brend tizimini qurib beramiz.",
 };
 
-const getBrands = async (): Promise<Brand[]> => {
-    // We are now returning a static list of brands.
-    // This is more reliable and faster.
-    // To add or remove brands, you can edit the `staticBrands` array above.
-    return staticBrands;
-}
-
-
-const Home: FC = async () => {
-  const brands = await getBrands();
+const Home: FC = () => {
+  const brands = staticBrands;
 
   return (
     <PageClient brands={brands} />
