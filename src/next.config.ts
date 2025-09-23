ed2f89e19f0d2a302bc4f0a85d1a79e2911da4b5
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-23 18:32:51 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/next.config.ts
+++ src/next.config.ts
@@ -60,4 +60,31 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
        {
-        protocol: 'https.
+        protocol: 'https',
+        hostname: 'img3.teletype.in',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.prod.website-files.com',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
