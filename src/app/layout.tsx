3a4b2c7eb55cf8e2bf01d6cfac7cf015f034a0f1
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-23 17:23:00 +0000

    <!-- Hotjar Tracking Code for Jon.Branding --> <script>     (function(h,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -168,7 +168,7 @@ const RootLayout: FC<Readonly<{ children: ReactNode }>> = ({ children }) => {
             __html: `
                 (function(h,o,t,j,a,r){
                     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:5084931,hjsv:6};
+                    h._hjSettings={hjid:6527829,hjsv:6};
                     a=o.getElementsByTagName('head')[0];
                     r=o.createElement('script');r.async=1;
                     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
