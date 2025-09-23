e9d8981d0b0c1ffe0d32969f204e9e13d99074d5
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-23 18:17:18 +0000

    saytni xavfsizlik darajasi o'ta yuqori bo'lishi kerak

---
+++ src/next.config.ts
@@ -0,0 +1,63 @@
+
+import type {NextConfig} from 'next';
+
+const securityHeaders = [
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+];
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  devIndicators: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+   async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img2.teletype.in',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img1.teletype.in',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'img4.teletype.in',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https.
