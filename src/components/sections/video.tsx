7b018033de0dc8904104420066f0a68fadb0fd21
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-24 11:47:03 +0000

    tushunarsiz bo'lib qolyapti video kattalashishi user pastga tushib qaytg

--- src/components/sections/video.tsx
+++ src/components/sections/video.tsx
@@ -13,8 +13,8 @@ const VideoSection = () => {
 
   const scale = useTransform(
     scrollYProgress,
-    [0, 0.5, 1],
-    [0.9, 1, 0.9]
+    [0, 0.4, 0.6, 1],
+    [0.9, 1, 1, 0.9]
   );
   
   const borderRadius = useTransform(
@@ -23,25 +23,19 @@ const VideoSection = () => {
     ["24px", "0px", "0px", "24px"]
   );
 
-  const maxWidth = useTransform(
-    scrollYProgress,
-    [0, 0.5, 1],
-    ['1280px', '100vw', '1280px']
-  );
-
   return (
     <section id="video" className="h-[200vh] bg-white" ref={ref}>
-      <div className="sticky top-0 h-screen flex items-center justify-center overflow-hidden">
+      <div className="sticky top-0 h-screen flex items-center justify-center overflow-hidden p-4 sm:p-8 md:p-12">
         <motion.div
-          className="relative w-full aspect-video shadow-2xl bg-black"
-          style={{ scale, borderRadius, maxWidth }}
+          className="relative w-full max-w-6xl aspect-video shadow-2xl bg-black"
+          style={{ scale, borderRadius }}
         >
           <iframe
             src="https://player.vimeo.com/video/1109613592?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&muted=1&loop=1&dnt=1&title=0&byline=0&portrait=0"
             frameBorder="0"
             allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
             className="absolute top-0 left-0 w-full h-full"
-            title="Blue and White Minimalist Goodbye Winter Video"
+            title="Jon.Branding Showreel"
             allowFullScreen
           ></iframe>
         </motion.div>
