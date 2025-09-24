1eec9d225f412cb30676af258485b70802a9521c
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-24 13:36:02 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 
 import type {NextConfig} from 'next';
 
+const securityHeaders = [
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+];
+
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
@@ -9,6 +24,14 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+   async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
   },
   images: {
     remotePatterns: [
