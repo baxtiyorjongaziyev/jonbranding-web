680c0e771d732aec6f62d2052b14944324ad5900
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-25 13:21:07 +0000

    button bo'yicha o'zimiz gradientli atrofida stroke ilonga o'xshab aylani

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,6 @@
 
+'use client';
+
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -53,7 +55,6 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, children, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
     
     if (asChild) {
       return (
