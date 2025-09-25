c4f8f9707e920d40287ebfaa975c58a2ab527589
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-25 18:48:01 +0000

    man saytimizda parallax effektini ishlayotganini sezmayapman.

--- src/app/xizmatlar/brand-strategy/page.tsx
+++ src/app/xizmatlar/brand-strategy/page.tsx
@@ -61,7 +61,7 @@ const BrandStrategyPage: FC = () => {
   return (
     <>
         <main className="flex-grow">
-        <Parallax speed={0.5}>
+        <Parallax speed={0.1}>
             <section className="py-20 sm:py-28 bg-white pt-32">
             <div className="container mx-auto px-4 text-center">
                 <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-dark-blue">
--- src/app/xizmatlar/neyming/page.tsx
+++ src/app/xizmatlar/neyming/page.tsx
@@ -51,7 +51,7 @@ const NamingPage: FC = () => {
   return (
     <>
         <main className="flex-grow">
-        <Parallax speed={0.5}>
+        <Parallax speed={0.1}>
             <section className="py-20 sm:py-28 bg-white pt-32">
             <div className="container mx-auto px-4 text-center">
                 <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-dark-blue">
