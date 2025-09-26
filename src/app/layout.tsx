2ea2d164ed287e9d491b135d17abd3528d06e16f
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-26 04:51:49 +0000

    404

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,7 @@
 
-'use client';
+import { ReactNode } from 'react';
+import './globals.css'
 
-import './globals.css';
-import { Toaster } from '@/components/ui/toaster';
-import type { FC, ReactNode } from 'react';
-import { Poppins } from 'next/font/google';
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
-});
-
-
-const RootLayout: FC<Readonly<{ children: ReactNode }>> = ({ children }) => {
-  return (
-    <html lang="uz" suppressHydrationWarning className={`${poppins.variable}`}>
-      <body>
-          {children}
-          <Toaster />
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return children;
 }
-
-export default RootLayout;
