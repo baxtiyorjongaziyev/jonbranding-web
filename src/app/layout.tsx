3aa1d603e0dfebebe3df7e985a33e46cb827f2ce
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-26 04:53:08 +0000

    Try fixing this error: `Runtime Error: Error: Missing <html> and <body>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
-
 import { ReactNode } from 'react';
 import './globals.css'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return children;
+  // Asosiy html va body teglari shu yerda bo'lishi kerak.
+  // [lang] layout esa o'zining ichki elementlarini boshqaradi.
+  return (
+    <html>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
 }
