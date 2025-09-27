9e15cbab05abe7569a5f7bb9206c4f2d84486680
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-27 13:02:11 +0000

    Please update the image here to the newly chosen ones (_for element img_

--- src/app/[lang]/xizmatlar/firmenniy-stil/page.tsx
+++ src/app/[lang]/xizmatlar/firmenniy-stil/page.tsx
@@ -112,7 +112,7 @@ const FirmenniyStilPage: FC = () => {
                         <Card className="shadow-xl rounded-2xl">
                             <CardContent className="p-0">
                             <Image 
-                                src="https://images.unsplash.com/photo-1603873908720-e56361eca7d2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwzfHxDb3Jwb3JhdGUlMjBpZGVudGl0eSUyMGlzJTIwYSUyMHN5c3RlbXxlbnwwfHx8fDE3NTg5Nzc5Nzh8MA&ixlib=rb-4.1.0&q=80&w=1080"
+                                src="https://images.unsplash.com/photo-1590102425712-1c28a0d6b85b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw3fHx2aXN1YWwlMjBpZGVudGl0eXxlbnwwfHx8fDE3NTg5NzgwMzl8MA&ixlib=rb-4.1.0&q=80&w=1080"
                                 width={800}
                                 height={600}
                                 data-ai-hint="design system grid"
