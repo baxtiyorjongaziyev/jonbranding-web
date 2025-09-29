61612c3f8beb1fa1e76edae1d3009c6fa785e260
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-09-29 06:47:47 +0000

    <!-- Google Tag Manager --> <script>(function(w,d,s,l,i){w[l]=w[l]||[];w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,12 +79,31 @@ const RootLayout: FC<Readonly<{ children: ReactNode, params: { lang: Locale } }>
     <html lang={lang} suppressHydrationWarning className={`${poppins.variable}`}>
       <head>
         <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5GRQBW84');`,
+          }}
+        />
+        <Script
           id="json-ld"
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
       </head>
       <body className="font-body bg-white antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5GRQBW84"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Script id="telegram-web-app" src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
         
         {/* Google Analytics */}
