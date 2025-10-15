e5eb18c4b253e71f8dc6acd19cca362965ae1f96
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-10-15 15:09:41 +0000

    Favicon nega desktopda ko'rinmayapti

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,6 +93,11 @@ export function generateMetadata({ params: { lang } }: { params: { lang: Locale 
       canonical: `${BASE_URL}/${lang === defaultLocale ? '' : lang}`,
       languages: alternates,
     },
+    icons: {
+      icon: 'https://img2.teletype.in/files/92/3c/923cd394-a437-47e1-86a1-51e1a2a3eb38.png',
+      shortcut: 'https://img2.teletype.in/files/92/3c/923cd394-a437-47e1-86a1-51e1a2a3eb38.png',
+      apple: 'https://img2.teletype.in/files/92/3c/923cd394-a437-47e1-86a1-51e1a2a3eb38.png',
+    },
     openGraph: {
       title,
       description,
