2dfca9b92738dddd150aedc14f2a65db5fc9dbbd
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-11-01 13:10:09 +0000

    <script>(function(a,m,o,c,r,m){a[m]={id:"436993",hash:"8761545509f209e11

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -151,7 +151,7 @@ const RootLayout: FC<Readonly<{ children: ReactNode, params: { lang: Locale } }>
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
-        <script dangerouslySetInnerHTML={{ __html: `(function(a,m,o,c,r,m){a[m]={id:"436565",hash:"18e8139df8663a8a487088b0dfb033008181860152438c8b7248cf2ad331e81c",locale:"ru",inline:true,setMeta:function(p){this.params=(this.params||[]).concat([p])}};a[o]=a[o]||function(){(a[o].q=a[o].q||[]).push(arguments)};a[o+'Config']=a[o+'Config']||{};a[o+'Config'].hidden=!0;var d=a.document,s=d.createElement('script');s.async=true;s.id=m+'_script';s.src='https://gso.amocrm.ru/js/button.js';d.head&&d.head.appendChild(s)})(window,0,'amoSocialButton',0,0,'amo_social_button');` }} />
+        <script dangerouslySetInnerHTML={{ __html: `(function(a,m,o,c,r,m){a[m]={id:"436993",hash:"8761545509f209e1154d24b2b1b57dfa1e78de77f34c8085c2297e1dddf2bfec",locale:"ru",inline:true,setMeta:function(p){this.params=(this.params||[]).concat([p])}};a[o]=a[o]||function(){(a[o].q=a[o].q||[]).push(arguments)};var d=a.document,s=d.createElement('script');s.async=true;s.id=m+'_script';s.src='https://gso.amocrm.ru/js/button.js';d.head&&d.head.appendChild(s)})(window,0,'amoSocialButton',0,0,'amo_social_button');` }} />
       </head>
       <body className="font-body bg-white antialiased">
         <MainLayout>
