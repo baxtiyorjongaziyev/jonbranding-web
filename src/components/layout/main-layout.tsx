3459925b2f338cf9d3222f23c593f5c7203b8e25
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-11-02 10:32:45 +0000

    bepul cookie platformalar bormi saytimiz bilan integratsiyasi ham bepul

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -35,6 +35,14 @@ function AnalyticsTracker() {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
+            
+            gtag('consent', 'default', {
+              'analytics_storage': 'denied',
+              'ad_storage': 'denied',
+              'ad_user_data': 'denied',
+              'ad_personalization': 'denied',
+            });
+
             gtag('config', '${GA_ID}');
           `,
         }}
