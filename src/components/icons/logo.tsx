7911a0138e1ab62a6c137f16721fa7df4f98cd67
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2025-12-11 12:17:55 +0000

    Zo'r aylana belgini olib tashlasang ham bo'ladi (_for element circle_)

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -2,11 +2,8 @@
 import { cn } from '@/lib/utils';
 
 export const Logo = ({ isWhite = false, className }: { isWhite?: boolean, className?: string }) => (
-    <div className={cn("flex items-center gap-2", className)}>
-        <svg height="32" viewBox="0 0 10 10" className="h-8 w-auto">
-            <circle cx="5" cy="5" r="5" fill="hsl(var(--primary))" />
-        </svg>
-        <div className="flex flex-col items-start">
+    <div className={cn("flex items-center", className)}>
+        <div className="flex flex-col items-center">
             <span className={cn("text-xl font-bold tracking-tighter leading-none", isWhite ? "text-white" : "text-dark-blue")}>
                 Jon.Branding
             </span>
