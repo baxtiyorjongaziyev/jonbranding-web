2822946226f03d553da2dfa0efa9a3dc0511a72e
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2026-03-20 06:19:26 +0000

    Try fixing this error: `Runtime TypeError: (0 , _lib_i18n_locale__WEBPAC

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -3,7 +3,7 @@ import { GoogleTagManager } from '@next/third-parties/google';
 import type { FC, ReactNode } from 'react';
 import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
-import { getDictionary, Locale } from '@/lib/i18n/locale';
+import { getDictionary, Locale } from '@/lib/dictionaries';
 import { locales, defaultLocale } from '@/lib/i18n/locale';
 import { Poppins } from 'next/font/google';
 import '../globals.css';
@@ -28,6 +28,13 @@ const LocalizedLayout: FC<Props> = async (props) => {
 
   return (
     <html lang={lang} suppressHydrationWarning className={poppins.variable}>
+      <head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+        <link rel="preconnect" href="https://www.googletagmanager.com" />
+        <link rel="preconnect" href="https://www.google-analytics.com" />
+        <link rel="dns-prefetch" href="https://www.google-analytics.com" />
+      </head>
       <body className="font-body bg-white antialiased" suppressHydrationWarning>
         <GoogleTagManager gtmId="GTM-5GRQBW84" />
         <Header lang={lang} dictionary={dictionary.header} />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,54 +31,46 @@ export const metadata: Metadata = {
 const RootLayout: FC<Readonly<{ children: ReactNode }>> = ({ children }) => {
   return (
     <>
-      <head>
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
-        <link rel="preconnect" href="https://www.googletagmanager.com" />
-        <link rel="preconnect" href="https://www.google-analytics.com" />
-        <link rel="dns-prefetch" href="https://www.google-analytics.com" />
-        <Script id="google-consent-mode" strategy="beforeInteractive">
-          {`
+      <Script id="google-consent-mode" strategy="beforeInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('consent', 'default', {
+            'ad_storage': 'granted',
+            'ad_user_data': 'granted',
+            'ad_personalization': 'granted',
+            'analytics_storage': 'granted'
+          });
+        `}
+      </Script>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-B3ZSKB40XY" strategy="afterInteractive"></Script>
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
-            gtag('consent', 'default', {
-              'ad_storage': 'granted',
-              'ad_user_data': 'granted',
-              'ad_personalization': 'granted',
-              'analytics_storage': 'granted'
-            });
-          `}
-        </Script>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-B3ZSKB40XY" strategy="afterInteractive"></Script>
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-B3ZSKB40XY', { page_path: window.location.pathname });
-              gtag('config', 'AW-17674872079');
-            `,
-          }}
-        />
-        {/* Meta Pixel Code */}
-        <Script id="fb-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1134785364752294');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-      </head>
+            gtag('js', new Date());
+            gtag('config', 'G-B3ZSKB40XY', { page_path: window.location.pathname });
+            gtag('config', 'AW-17674872079');
+          `,
+        }}
+      />
+      <Script id="fb-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1134785364752294');
+          fbq('track', 'PageView');
+        `}
+      </Script>
       <MainLayout>
         {children}
       </MainLayout>
