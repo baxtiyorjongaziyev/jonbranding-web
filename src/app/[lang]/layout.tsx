7e2677a1a202aacb306637cf5cdd1ec9463ab307
Baxtiyorjon Gaziyev <baxtiyorjongaziyev@gmail.com>
2026-03-23 09:32:39 +0500

    feat: add Yandex Metrika and continue SSR 500 diagnostics

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -118,10 +118,28 @@ export default async function LocalizedLayout({ children, params }: Props) {
           </div>
           <Footer lang={lang} dictionary={dictionary.footer} />
           <StickyCTA />
+          {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+            ym(91628105, 'init', {webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/91628105" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
+        
           <LeadMagnetPopup dictionary={(dictionary as any).leadMagnetPopup} />
         </MainLayout>
       </body>
     </html>
   );
 }
-
