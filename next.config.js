1a0c4c4843352859422f263b33eb2cf86018fc13
Baxtiyorjon Gaziyev <58423710+baxtiyorjongaziyev@users.noreply.github.com>
2026-06-17 22:47:14 +0500

    fix(security): frame-ancestors 'self' to match X-Frame-Options SAMEORIGIN

--- next.config.js
+++ next.config.js
@@ -116,7 +116,7 @@ const nextConfig = {
               "object-src 'none'",
               "base-uri 'self'",
               "form-action 'self'",
-              "frame-ancestors 'none'",
+              "frame-ancestors 'self'",
               "upgrade-insecure-requests",
             ].join('; '),
           },
