c064c023f0411414ece56582dab2701bb8bc6a84
Baxtiyorjon Gaziyev <58423710+baxtiyorjongaziyev@users.noreply.github.com>
2026-06-28 10:10:36 +0500

    fix: duplicate import in blog slug page

--- src/app/[lang]/blog/[slug]/page.tsx
+++ src/app/[lang]/blog/[slug]/page.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Script from 'next/script';
 import { getDictionary, Locale } from '@/lib/dictionaries';
-import { getDictionary, Locale } from '@/lib/dictionaries';
 import { client } from '@/sanity/lib/client';
 import { PortableText } from '@portabletext/react';
 
